9dc06d696ea60acd55ab5122b0a5c02f5ce841c0
Smriti Nayak <131101942+smritinayak5@users.noreply.github.com>
2023-07-18 06:56:02 +0530

    searchoptions.php

---
+++ public/Browsers/searchoptions.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Search Options</title>
+        <link rel="stylesheet" href="browsers.css">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    </head>
+    <body>
+        <h1 style="color: whitesmoke; text-align: center; margin-top: 20px; font-family: serif; font-weight: bold; font-size: 80px; text-transform: uppercase;">Browsers</h1>
+        <div class="icons">
+            <div class="row">
+                <div class="coloum">
+                    <div class="site" title="Google Chrome">
+                        <a id='gfg' href="https://www.google.com">
+                            <input class="button" type="image" src="Chrome.jpg" height="110px"/>
+                            <p>Google Chrome</p>
+                        </a>
+                    </div>
+                </div>
+                <div class="coloum">
+                    <div class="site" title="Microsoft Edge">
+                        <a id='gfg' href="https://www.microsoft.com">
+                            <input class="button" type="image" src="edge.png" height="100px"/>
+                            <p>Microsoft Edge</p>
+                        </a>
+                    </div>
+                </div>
+                <div class="coloum">
+                    <div class="site" title="Firefox">
+                        <a id='gfg' href="https://www.mozilla.org/en-US/firefox/all/">
+                            <input class="button" type="image" src="firefox.png" height="100px"/>
+                            <p>Firefox</p>
+                        </a>
+                    </div>
+                </div>
+                <div class="coloum">
+                    <div class="site" title="Safari">
+                        <a id='gfg' href="https://support.apple.com">
+                            <input class="button" type="image" src="Safari.png" height="100px"/>
+                            <p>Safari</p>
+                        </a>
+                    </div>
+                </div>
+                <div class="coloum">
+                    <div class="site" title="Opera">
+                    <a id='gfg' href="https://forums.opera.com">
+                            <input class="button" type="image" src="Opera.png" height="100px"/>
+                        <p>Opera</p>
+                    </div>
+                </div>
+                <div class="coloum">
+                    <div class="site1" title="YouTube">
+                        <a id='gfg' href="https://www.youtube.com">
+                            <i class="fa fa-youtube-play"></i>
+                            <p>YouTube</p>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
