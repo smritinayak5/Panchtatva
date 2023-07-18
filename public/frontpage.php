cc4313b6bc11885703770f5fd53697b92b5a2bf7
Smriti Nayak <131101942+smritinayak5@users.noreply.github.com>
2023-07-18 06:48:38 +0530

    frontpage.php

---
+++ public/frontpage.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Panchtatva</title>
+        <link rel="stylesheet" href="style.css">
+
+    </head>
+    <body>
+        <center>
+            <div class="col">
+                <div class="site" title="Air">
+                    <a id='gfg' href="login.php">
+                        <input class="button" type="image" src="BG/air.jpg" height="100px"/>
+                        <p>Musics</p>
+                    </a>
+                </div>
+            </div>
+        </center>
+        <center>
+            <div class="col1">
+                <div class="site" title="Fire">
+                    <a id='gfg' href="login.php">
+                        <input class="button" type="image" src="BG/fire.jpg" height="100px"/>
+                        <p>Games</p>
+                    </a>
+                </div>
+            </div>
+            <div class="col1">
+                <div class="site" title="land">
+                    <a id='gfg' href="login.php">
+                        <input class="button" type="image" src="BG/land.jpg" height="100px"/>
+                        <p>Stories</p>
+                    </a>
+                </div>
+            </div>
+        </center>
+        <center>
+            <div class="col2">
+                <div class="site" title="Water">
+                    <a id='gfg' href="login.php">
+                        <input class="button" type="image" src="BG/water.jpg" height="100px"/>
+                        <p>Search</p>
+                    </a>
+                </div>
+            </div>
+            <div class="col2">
+                <div class="site" title="Cloud">
+                    <a id='gfg' href="login.php">
+                        <input class="button" type="image" src="BG/cloud.jpg" height="100px"/>
+                        <p>Wether Forcast</p>
+                    </a>
+                </div>
+            </div>
+        </center>
+    </body>
+</html>
